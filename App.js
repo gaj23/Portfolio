03a4a5c31f19368c3a149a6a814647f0aab85b95
Gabrielle Joyce <68332132+gaj23@users.noreply.github.com>
2021-06-16 14:40:57 -0400

    Publish with expo and add comments for knowhow

--- App.js
+++ App.js
@@ -1,16 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// we don't use html elements, but rather the components provided by react native
 
-export default function App() {
+const App = () => {
+  console.log('logging is possible!!')
+  // logs can be viewed in metro bundler after selecting the "device" your simulating under
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>ga-bri-elle</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+export default App;
+
+// here we create a style sheet object
+// we pass in an object with a property
+// we use the JS for CSS attributes which will be translated on build
 const styles = StyleSheet.create({
   container: {
     flex: 1,
